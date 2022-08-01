4b6673ad4e9ca7871349fd552d141f7dff501a50
klappy <christopher@klapp.name>
2022-08-01 16:02:17 -0400

    refactor

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { getCaretIndexFromElement } from "./getCaretIndexFromElement";
+
+export function addArrowKeyNavigationToContenteditableElements({ verbose }) {
+  const elements = getContenteditableElements();
+
+  addArrowKeyNavigationToElements({ elements, verbose })
+};
+
+export function getContenteditableElements() {
+  const contenteditableElements = document.querySelectorAll('[contenteditable]');
+  return contenteditableElements;
+};
+
+export function addArrowKeyNavigationToElements({ elements, verbose }) {
+  [...elements].forEach(element => {
+    addArrowKeyNavigationToElement({ element, verbose });
+  });
+};
+
+export function addArrowKeyNavigationToElement({ element, verbose=false }) {
+  element.addEventListener("keydown", (event) => {
+    const { key, target } = event;
+    const position = getCaretIndexFromElement({ element: target });
+    const length = event.target.innerText.length;
+    if (verbose) console.log({ key, position, length });
+    
+    if (key === "ArrowUp" && position === 0){
+      event.target.previousElementSibling.focus();
+    };
+    
+    if (key === "ArrowDown" && position === length){
+      event.target.nextElementSibling.focus();
+    };
+  });
+};
+
+
