3f3bd5600ef3dfcda8f0ccaae0380239329643b7
klappy <christopher@klapp.name>
2022-08-02 17:39:57 -0400

    added parcel, local server, fix whitespace/linebreaks

---
+++ public/index.js
@@ -0,0 +1,4 @@
+import { addArrowKeyNavigationToContenteditableElements } from "../src/index.js";
+import "./styles.css";
+
+addArrowKeyNavigationToContenteditableElements({verbose: true});
--- src/index.js
+++ src/index.js
@@ -22,11 +22,14 @@ export function addArrowKeyNavigationToElement({ element, verbose=false }) {
   element.addEventListener("keydown", (event) => {
     const { key, target } = event;
     const position = getCaretIndexFromElement({ element: target });
+    const string = event.target.textContent;
+    // get the whitespace offset in case it is not rendered.
+    const whitespaceOffset = string.match(/^\s*/)[0].length;
     // to get length, with and without prewrap to work, trim unrendered final linebreak
-    const length = event.target.innerText.replace(/\n$/, '').length;
-    if (verbose) console.log({ key, position, length });
+    const length = string.length - whitespaceOffset;
+    if (verbose) console.log({ key, position, whitespaceOffset, length, string });
     
-    if (key === "ArrowUp" && position === 0){
+    if (key === "ArrowUp" && position <= whitespaceOffset){
       event.target.previousElementSibling?.focus();
     };
     // position may excede length when pre-wrap is not used
